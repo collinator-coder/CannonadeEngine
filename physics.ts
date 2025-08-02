0b02b02f98e123235dc02097a9ea558fbcfcc512
collinator-coder <crphillips2012@gmail.com>
2025-08-02 12:13:24 -0700

    implement vector and pointmass classes

--- physics.ts
+++ physics.ts
@@ -2,8 +2,7 @@
  * Provides access to basic micro:bit functionality.
  */
 //% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
-namespace physics
-{
+namespace physics {
     export class Vector {
         constructor(x: number, y: number) {
             this.x = x;
@@ -11,4 +10,17 @@ namespace physics
         }
         x: number;
         y: number;
+    }
+    export class PointMass {
+        constructor(x: number, y: number, mass: number) {
+            this.x = x;
+            this.y = y;
+            this.mass = mass;
+        }
+        x: number;
+        y: number;
+        vx: number;
+        vy: number;
+        mass: number;
+    }
 }
